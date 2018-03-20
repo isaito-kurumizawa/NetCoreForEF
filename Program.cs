2dd14d76c6e41f6a75b4af780dfd3aa1ff359d10
isaito-kurumizawa <exkurumizawa@gmail.com>
2018-03-20 11:48:27 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace NetCoreForEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                var test = dbContext._test.ToList();   
+            }
+        }
+    }
+}
